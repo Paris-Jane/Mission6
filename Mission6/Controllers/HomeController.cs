391bcfd4a9438d0e968e38068620f7e033fa87c1
Paris-Jane <parisjan@byu.edu>
2026-02-13 12:53:43 -0700

    base

---
+++ Mission6/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mission6.Models;
+
+namespace Mission6.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    public IActionResult Collection()
+    {
+        return View();
+    }
+    
+    public IActionResult About()
+    {
+        return View();
+    }
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
